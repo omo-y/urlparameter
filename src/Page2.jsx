1cd3869463ddfae277e11566d968a27a6403cb42
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-26 14:20:24 +0900

    urlparameter1

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,6 +4,9 @@ export const Page2 = () => {
     <div>
       <h1>Page2</h1>
       <Link to="/page2/1009">URL Parameter</Link>
+      <br />
+      <br />
+      <Link to="/page2/1009?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/URLparameter.jsx
+++ src/URLparameter.jsx
@@ -1,10 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 export const URLparameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>URLparameter</h1>
       <h2>パラメーター {id}</h2>
+      <h2>クエリパラメーター{query.get("name")}</h2>
     </div>
   );
 };
