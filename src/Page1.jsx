f14ef51adb75ba1d35ec94c29f35ade0c46cf8e0
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-26 16:48:04 +0900

    oCLicksenni

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
+  const history = useHistory();
 
+  const onClickDetailA = () => history.push("/Page1/DetailA");
   return (
     <div>
       <h1>Page1</h1>
@@ -9,6 +11,9 @@ export const Page1 = () => {
       <br />
       <br />
       <Link to="Page1/DetailB">DetailB</Link>
+      <br />
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,16 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const location = useLocation();
   console.log(location);
+  const history = useHistory();
+  const kaeru = () => {
+    history.goBack();
+  };
   return (
     <div>
       <h1>Page1DetailAです</h1>
+      <button onClick={kaeru}>戻る</button>
     </div>
   );
 };
